87c39f121135026d3c988a715cd6a74654a54af1
Rysiek <ryszard.kozmik@gmail.com>
2013-08-15 07:50:17 +0200

    Improved a bit breadcrumb code.

--- _ext/breadcrumb.rb
+++ _ext/breadcrumb.rb
@@ -1,10 +1,32 @@
+##
+#
+# Awestruct::Extensions:Breadcrumb is a helper type of awestruct extension.
+# When used in Haml template, it will generate a breadcrumb navigation.
+#
+# Configuration:
+#
+# 1. configure the extension in the project pipeline.rb:
+#    - add breadcrumb dependency:
+#
+#      require 'breadcrumb'
+#
+#    - add the extension initialization:
+#
+#      helper Awestruct::Extensions::Breadcrumb
+#
+# 2. In your Haml layout add:
+#
+#    = breadcrumb(page.output_path)
+#
+##
+
 module Awestruct
   module Extensions
     module Breadcrumb
 
       def breadcrumb(path)
 
-        return nil if !path or path.eql?("/") or path.eql?("index.html")
+        return nil if !path or path.eql?("/") or path.eql?("/index.html")
 
         output = ""
         index = -1
@@ -28,7 +50,7 @@ module Awestruct
           output += generateAnchorHtml(findInPages(path))
         end
 
-        # Returning output content without trailing arrow and wrapped in a div.
+        # Returning output content without trailing slash and wrapped in a div.
         output = "<div class='breadcrumb'>" + output[0..output.length-4] + "</div>"
       end
 
@@ -40,18 +62,16 @@ module Awestruct
             return page
           end
         end
-
+        nil
       end
 
       def generateAnchorHtml(page)
 
-        page = page.real_page if page.real_page != nil
-
-        path = page.output_path == nil ? page.url : page.output_path
+        path = (page.output_path.nil? ? page.url : page.output_path)
 
         return "" if path==nil
 
-        "<a class='breadcrumb_anchor' href='#{site.base_url}#{page.output_path}'
+        "<a class='breadcrumb_anchor' href='#{site.base_url}#{path}'
         >#{page.title ? page.title : page.simple_name.capitalize }</a> / "
 
       end
