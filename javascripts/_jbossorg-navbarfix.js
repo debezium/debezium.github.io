85596fda1eac9036bbe552656631b2224769d2cc
Rysiek <ryszard.kozmik@gmail.com>
2014-06-30 01:09:46 +0200

    Correcting comments and clearing the code of the sticky navbar.

--- javascripts/_jbossorg-navbarfix.js
+++ javascripts/_jbossorg-navbarfix.js
@@ -1,12 +1,12 @@
 /*
- * This script prevents a navbar with the #navbar-fix identifier
+ * This script prevents a navbar with the #sticky-navbar identifier
  * from scrolling off the top of the browser window. It does this by
- * detecting when that's about to occur and creating a copy that's
- * added to the page with a fixed position, aligned to the top of
+ * detecting when that's about to occur and switching a style class
+ * which sets it at a fixed position, aligned to the top of
  * the page.
  *
- * When the user scrolls back up then this copy is
- * removed so the normal navbar is fully visible again.
+ * When the user scrolls back up then this style class is switched
+ * back so the normal navbar is fully visible again.
  *
  * Because you can't scroll the fixed copy we're not able to use this
  * for the collapsed navigation where menu items are shown vertically.
@@ -14,7 +14,7 @@
 
 var isNavBarFixed = 0;
 
-var defaultNavbarOffset = $("#sticky-navbar").offset().top;
+var defaultNavbarOffset = $("#sticky-navbar").length ? $("#sticky-navbar").offset().top : 0 ;
 
 processScroll();
 $(window).on('scroll', processScroll);
@@ -22,19 +22,23 @@ $(window).on('scroll', processScroll);
 function processScroll() {
   
   var navbar = $("#sticky-navbar");
-  var breadcrumb = $(".breadcrumb");
-
-  if (isNavBarFixed) {
-    breadcrumb = $(".breadcrumb-fixed");
-  }
-
-  if (navbar == null || typeof (navbar.offset()) == "undefined" ) {
+  
+  if (navbar.length==0) {
     return
   }
 
+  var breadcrumb;
+
+  if (isNavBarFixed) {
+    breadcrumb = $(".breadcrumb-fixed");
+  } else {
+    breadcrumb = $(".breadcrumb");
+  }
+
+  // Measuring additionall offset depending whether tabzilla exists and is open.
   var additionalTabzillaOffset = 0;
   var tabzilla = $('#tabnav-panel');
-  if (typeof tabzilla != undefined) {
+  if (tabzilla.length) {
     if (tabzilla.hasClass('tabnav-opened')) {
       additionalTabzillaOffset=240;
     }
@@ -42,16 +46,27 @@ function processScroll() {
   
   // Tabzilla offset needs to bo added if it's open.
   if (!isNavBarFixed && $(window).scrollTop() >= (defaultNavbarOffset + additionalTabzillaOffset) ) {
+
+    // Switching navbar style to fixed position at the top.
     navbar.addClass("navbar-fixed");
     navbar.removeClass("navbar-fix");
+
+    // Trick in order to prevent content movement when the navigation starts to scroll.
     breadcrumb.addClass("breadcrumb-fixed");
     breadcrumb.removeClass("breadcrumb");
+
     isNavBarFixed = 1;
+
   } else if (isNavBarFixed && $(window).scrollTop() < (defaultNavbarOffset + additionalTabzillaOffset) ) {
+    
+    // Switching navbar style to non-fixed position.
     navbar.addClass("navbar-fix");
     navbar.removeClass("navbar-fixed");
+
     breadcrumb.removeClass("breadcrumb-fixed");
     breadcrumb.addClass("breadcrumb");
-    isNavBarFixed = 0
+
+    isNavBarFixed = 0;
+
   }
 }
