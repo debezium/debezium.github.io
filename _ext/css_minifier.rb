9f669438bde5efbc91132e15bf8bb3515c9c489b
Xavier Coulon <xcoulon@redhat.com>
2014-09-19 15:14:37 +0200

    Fixing minifiers when file has no extension

--- _ext/css_minifier.rb
+++ _ext/css_minifier.rb
@@ -42,37 +42,39 @@ module Awestruct
 
         # Test if it's a CSS file.
         ext = File.extname(page.output_path)
-        if !ext.empty?
+        
+        # skip if the file has no extension
+        if ext.empty?
+          return input
+        end
+        
+        ext_txt = ext[1..-1]
 
-          ext_txt = ext[1..-1]
-
-          # Filtering out non-css files and those which were already minimized with added suffix.
-          if ext_txt == "css" and !page.output_path.to_s.end_with?("min.css")
-            print "Minifying css #{page.output_path} \n"
-            output = CSSminify.compress(input)
-          else
-            return input
-          end
+        # Filtering out non-css files and those which were already minimized with added suffix.
+        if ext_txt == "css" and !page.output_path.to_s.end_with?("min.css")
+          print "Minifying css #{page.output_path} \n"
+          output = CSSminify.compress(input)
+        else
+          return input
+        end
 
         oldFileName = File.basename(page.output_path).to_s
 
-          # Create new file name with suffix added
-          newFileName = oldFileName.slice(0..oldFileName.length-4)+"min.css"
-          newOutputPath = File.join(File.dirname(page.output_path.to_s),newFileName)
+        # Create new file name with suffix added
+        newFileName = oldFileName.slice(0..oldFileName.length-4)+"min.css"
+        newOutputPath = File.join(File.dirname(page.output_path.to_s),newFileName)
 
-          # Create a temporary file with the merged content.
-          tmpOutputPath = File.join( "./_tmp/" , newFileName)
-          tmpOutputFile = File.new(tmpOutputPath,"w")
-          tmpOutputFile.write(output)
-          tmpOutputFile.close
+        # Create a temporary file with the merged content.
+        tmpOutputPath = File.join( "./_tmp/" , newFileName)
+        tmpOutputFile = File.new(tmpOutputPath,"w")
+        tmpOutputFile.write(output)
+        tmpOutputFile.close
 
-          # Add the temporary file to the list of pages for rendering phase.
-          newPage = site.engine.load_page(tmpOutputPath)
-          newPage.source_path = tmpOutputPath
-          newPage.output_path = newOutputPath
-          site.pages << newPage
-
-        end
+        # Add the temporary file to the list of pages for rendering phase.
+        newPage = site.engine.load_page(tmpOutputPath)
+        newPage.source_path = tmpOutputPath
+        newPage.output_path = newOutputPath
+        site.pages << newPage
 
         # We return the input because we leave the original file untouched
         input
