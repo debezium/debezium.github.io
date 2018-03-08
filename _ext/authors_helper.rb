5d63618c9b1780ed5d678109ed493cbbf0a4d370
Gunnar Morling <gunnar.morling@googlemail.com>
2018-03-08 22:24:42 +0100

    Allowing multiple authors per post

--- _ext/authors_helper.rb
+++ _ext/authors_helper.rb
@@ -1,7 +1,12 @@
 module Awestruct::Extensions::AuthorsHelper
-  def lookup_name(nick)
-    author = site.authors[nick]
-    name = author ? author["name"] : nick
-    name ? name : "Unknown"
+  def lookup_name(nicks)
+    names = nicks.split(",").map { |nick|
+      nick = nick.strip
+      author = site.authors[nick]
+      name = author ? author["name"] : nick
+      name ? name : "Unknown"
+    }
+
+    names.join(", ")
   end
 end
