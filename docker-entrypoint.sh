f1095f770082f2fef8f982de9f895e1732f8e845
uidoyen <asansari@localhost.localdomain>
2020-10-28 16:35:32 +0530

    Implementing Docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ ! -f Gemfile ]; then
+  echo "NOTE: hmm, I don't see a Gemfile so I don't think there's a jekyll site here"
+  echo "Either you didn't mount a volume, or you mounted it incorrectly."
+  echo "Be sure you're in your jekyll site root and use something like this to launch"
+  echo ""
+  echo "docker run -p 80:4000 -v \$(pwd):/site uidoyen/jekyll-serve"
+  echo ""
+  echo "NOTE: To create a new site, you can use the sister image uidoyen/jekyll like:"
+  echo ""
+  echo "docker run -v \$(pwd):/site uidoyen/jekyll new ."
+  exit 1
+fi
+
+bundle install --retry 5 --jobs 20
+
+exec "$@"
