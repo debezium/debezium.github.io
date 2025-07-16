ad152d1d77891da39ca15ff48d512ed431cbf5ac
Fiore Mario Vitale <mvitale@redhat.com>
2025-07-16 16:03:37 +0200

    DBZ-9227 Fix LightBox js to be ES5 compliant

--- assets/javascript/lightbox.js
+++ assets/javascript/lightbox.js
@@ -1,18 +1,19 @@
 // Add this script to your site's JavaScript file or before </body>
 document.addEventListener('DOMContentLoaded', function() {
-    const modal = document.getElementById('imageModal');
-    const modalImage = document.getElementById('modalImage');
-    const modalCaption = document.getElementById('modalCaption');
-    const closeBtn = document.querySelector('.close');
+    var modal = document.getElementById('imageModal');
+    var modalImage = document.getElementById('modalImage');
+    var modalCaption = document.getElementById('modalCaption');
+    var closeBtn = document.querySelector('.close');
 
     // Add click event to all responsive images
-    document.querySelectorAll('.responsive-image').forEach(img => {
-        img.addEventListener('click', function() {
+    var images = document.querySelectorAll('.responsive-image');
+    for (var i = 0; i < images.length; i++) {
+        images[i].addEventListener('click', function() {
             modal.style.display = 'block';
             modalImage.src = this.src;
             modalCaption.textContent = this.alt;
         });
-    });
+    }
 
     // Close modal when clicking the X
     closeBtn.addEventListener('click', function() {
